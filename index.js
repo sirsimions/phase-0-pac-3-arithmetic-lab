bf726bb5a70177ec43b2d90f89cfddcdde010844
Simion Okola <simion.okola@moringaschool.com>
2022-08-04 23:24:01 +0300

    Complete assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+var a, b;
+function add(a, b){
+    return a+b;
+}
+function subtract(a, b){
+    return a-b;
+}
+function multiply(a, b){
+    return a*b
+}
+function divide(a, b){
+    return a/b;
+}
+function increment(a){
+    return (a = a+1);
+}
+function decrement(a){
+    return (a = a-1);
+}
+function makeInt(a){
+    return (a = parseInt(a, 10))
+}
+function preserveDecimal(a){
+     return parseFloat(a);
+}
